6bae32c6e57c60e0806f88e7a7dba7b3d3889b3d
Julien Meslin <julien.meslin@epitech.eu>
2019-01-28 18:06:34 +0100

    Crawler manager done, template library done with exemple crawlers

---
+++ Crawler/WebCrawlerManager/ClassLibrary1/FramePerfectCrawlerHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FramePerfectCrawlerLib
+{
+    class FramePerfectCrawlerHandler
+    {
+        public List<IWebCrawler> GetCharacterFromCrawlers()
+        {
+            List<IWebCrawler> crawlers = new List<IWebCrawler>();
+            crawlers.Add(new RbnorWebCrawler());
+            crawlers.Add(new DustLoopCrawler());
+            return crawlers;
+        }
+    }
+}
